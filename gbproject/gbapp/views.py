d8f22a1ff78a4a1a704d56a62e6932f5422e07bc
dvalchibaev <alchibaevdv@gmail.com>
2023-08-28 17:10:10 +0400

    lesson01 update

--- gbproject/gbapp/views.py
+++ gbproject/gbapp/views.py
@@ -1,12 +1,19 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
+logger = logging.getLogger(__name__)
+
+
 def index(request):
+    logger.info('Index page accessed')
     return HttpResponse("Hello, World!")
 
 
 def about(request):
+    logger.info('About page accessed')
     return HttpResponse("About us")
+
 
 
